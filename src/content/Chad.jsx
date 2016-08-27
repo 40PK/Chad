c03071f9e88f116287a9cf42497ad13de58313bf
Perkovec <perkovec24@gmail.com>
2016-08-27 17:15:42 +0300

    Fix lint

--- src/content/Chad.jsx
+++ src/content/Chad.jsx
@@ -1,4 +1,6 @@
-const { Paper, List, Snackbar } = require('material-ui');
+/* eslint-disable no-alert */
+const React = require('react');
+const { Paper, Snackbar } = require('material-ui');
 const { Layout, Fixed, Flex } = require('react-layout-pane');
 const { ipcRenderer } = require('electron');
 const { MuiThemeProvider, getMuiTheme } = require('material-ui/styles');
@@ -7,19 +9,18 @@ const lightBaseTheme = require('material-ui/styles/baseThemes/lightBaseTheme').d
 const SideBar = require('./components/SideBar');
 const ContentBar = require('./components/ContentBar');
 const langs = {
+  /* eslint-disable global-require */
   ru: require('./langs/ru'),
   en: require('./langs/en'),
+  /* eslint-enable global-require */
 };
 
 const TGAPI = require('./js/API');
 const Signal = require('./js/Signal');
 const Utils = require('./js/Utils');
-const ua = require('universal-analytics');
 
 const usernameRegex = /[@-].{5,}/;
 const tokenRegex = /\d{9}:.{30,}/;
-
-let analytics = ua('UA-81643761-1', { https: true });
 
 const tags = {
   paperStyle: { height: '100%', width: 256 },
@@ -69,79 +70,199 @@ class Chad extends React.Component {
     this.closeSnackbar = this.closeSnackbar.bind(this);
   }
 
-  languageChange(value) {
-    let state = this.state;
-    state.settings.lang = value;
-    localStorage.setItem('settings', JSON.stringify(state.settings));
-    this.local = langs[value];
-    this.setState(state);
-    ipcRenderer.send('build-menu', this.local);
+  getPostIndexByUid(uid) {
+    for (let i = 0; i < this.state.posts.length; ++i) {
+      if (this.state.posts[i].uid === uid) return i;
+    }
+    return null;
   }
 
-  darkThemeChange(value) {
-    let state = this.state;
-    state.settings.darkTheme = value;
-    localStorage.setItem('settings', JSON.stringify(state.settings));
-    this.setState(state, this.props.deepForceUpdate);
+  getDraftIndexByUid(uid) {
+    for (let i = 0; i < this.state.drafts.length; ++i) {
+      if (this.state.drafts[i].uid === uid) return i;
+    }
+    return null;
   }
 
-  postWriteDefaultsChange(value) {
-    let state = this.state;
-    state.settings.postWriteDefaults = value;
-    localStorage.setItem('settings', JSON.stringify(state.settings));
-    this.setState(state);
-  }
+  setAdminBot(token, onPass) {
+    if (!token || token.length < 30) {
+      return alert(this.loca.alert_invalid_token);
+    }
 
-  makeSnackbar(text) {
-    this.setState({
-      snackbar: {
-        open: true,
-        text: text,
-      },
+    if (!tokenRegex.test(token)) {
+      return alert(this.local.alert_invalid_token_format);
+    }
+
+    if (onPass) onPass();
+
+    this.API = new TGAPI(token);
+    const API = this.API;
+
+    const signal = this.signal;
+    signal.call('SetLoadState', [true]);
+
+    API.getMe().then(res => {
+      const getMe = res.body;
+      if (getMe.ok) {
+        getMe.result.token = token;
+
+        localStorage.setItem('bot', JSON.stringify(getMe.result));
+        this.token = token;
+        this.setState({
+          bot: getMe.result,
+        }, () => signal.call('SetLoadState', [false]));
+        this.updateBotProfilePhoto(getMe.result.id);
+      }
+    }, () => {
+      signal.call('SetLoadState', [false]);
+      alert(this.local.alert_cant_find_bot);
     });
+
+    return null;
   }
 
-  closeSnackbar() {
-    this.setState({
-      snackbar: {
-        open: false,
-        text: '',
-      },
+  removeChannel(id, onRemove) {
+    const channels = this.state.channels;
+
+    for (let i = 0; i < channels.length; ++i) {
+      if (channels[i].uid === id) {
+        channels.splice(i, 1);
+        return this.setState({
+          channels,
+        }, () => {
+          localStorage.setItem('channels', JSON.stringify(channels));
+          onRemove();
+        });
+      }
+    }
+
+    return null;
+  }
+
+  newChannel(data) {
+    if (!data.name || data.name.length === 0) {
+      return alert(this.local.alert_channel_name_short);
+    }
+
+    if (!data.username || !usernameRegex.test(data.username)) {
+      return alert(this.local.alert_invalid_channel_name);
+    }
+
+    const channels = this.state.channels;
+    channels.push({
+      name: data.name,
+      username: data.username,
+      uid: Utils.uid(),
     });
+
+    this.setState({
+      channels,
+    }, () => localStorage.setItem('channels', JSON.stringify(channels)));
+
+    return null;
   }
 
-  sendPost(data) {
-    let state = this.state;
-    let listToSend = this.signal.call('SelectedChannels');
-
-    if (listToSend.length < 1)
-      return alert(this.local.alert_select_channel_to_send);
-
-    if (this.token === null)
+  changePost(data) {
+    if (this.token === null) {
       return alert(this.local.alert_add_admin_bot);
+    }
 
-    if (data.text.length < 1)
-      return alert(this.local.alert_post_text_empty);
+    if (data.text.length < 1) {
+      return alert(this.loca.alert_post_text_empty);
+    }
 
     data.onStart.apply(null);
 
-    let params = {
+    const params = {
+      text: data.text,
+      disable_web_page_preview: data.disablePreview,
+      parse_mode: data.parser,
+    };
+
+    const listToSend = data.post.chats.slice(0);
+
+    const API = this.API;
+    function sendRec(list, onend) {
+      params.chat_id = list[0].chat_id;
+      params.message_id = list[0].message_id;
+
+      if (typeof params.chat_id === 'string') {
+        params.chat_id = `@${params.chat_id}`;
+      }
+
+      API.editMessageText(params).then(res => {
+        const success = res.body;
+        if (success.ok) {
+          list.splice(0, 1);
+          if (list.length) {
+            sendRec(list, onend);
+          }
+        } else {
+          alert(this.local.alert_something_wrong);
+          onend(success);
+        }
+
+        if (!list.length) {
+          onend(success);
+        }
+      });
+    }
+
+    sendRec(listToSend, (success) => {
+      data.onEnd.apply(null);
+      const post = data.post;
+      post.date = success.result.date;
+      post.edit_date = success.result.edit_date || null;
+      post.text = data.text || null;
+      post.parse_mode = params.parse_mode;
+      post.disableNotification = params.disable_notification;
+      post.disablePreview = params.disable_web_page_preview;
+      const posts = this.state.posts;
+      posts[this.getPostIndexByUid(data.post.uid)] = post;
+      localStorage.setItem('posts', JSON.stringify(posts));
+      this.setState({
+        posts,
+      });
+    });
+
+    return null;
+  }
+
+  sendPost(data) {
+    const state = this.state;
+    const listToSend = this.signal.call('SelectedChannels');
+
+    if (listToSend.length < 1) {
+      return alert(this.local.alert_select_channel_to_send);
+    }
+
+    if (this.token === null) {
+      return alert(this.local.alert_add_admin_bot);
+    }
+
+    if (data.text.length < 1) {
+      return alert(this.local.alert_post_text_empty);
+    }
+
+    data.onStart.apply(null);
+
+    const params = {
       text: data.text,
       disable_web_page_preview: data.disablePreview,
       disable_notification: data.disableNotification,
       parse_mode: data.parser,
     };
 
-    let post = {
+    const post = {
       chats: [],
     };
 
-    let API = this.API;
+    const API = this.API;
     function sendRec(list, onend) {
       params.chat_id = list[0];
 
-      API.sendMessage(params).then((success) => {
-        success = success.body;
+      API.sendMessage(params).then(res => {
+        const success = res.body;
         if (success.ok) {
           post.chats.push({
             name: success.result.chat.title || '',
@@ -178,164 +299,61 @@ class Chad extends React.Component {
       localStorage.setItem('posts', JSON.stringify(state.posts));
       this.setState(state);
     });
-
-    analytics.event('Main events', 'Used parser: ' + data.parser).send();
+    return null;
   }
 
-  changePost(data) {
-    if (this.token === null)
-      return alert(this.local.alert_add_admin_bot);
-
-    if (data.text.length < 1)
-      return alert(this.loca.alert_post_text_empty);
-
-    data.onStart.apply(null);
-
-    let params = {
-      text: data.text,
-      disable_web_page_preview: data.disablePreview,
-      parse_mode: data.parser,
-    };
-
-    let listToSend = data.post.chats.slice(0);
-
-    let API = this.API;
-    function sendRec(list, onend) {
-      params.chat_id = list[0].chat_id;
-      params.message_id = list[0].message_id;
-
-      if (typeof params.chat_id === 'string')
-        params.chat_id = '@' + params.chat_id;
-
-      API.editMessageText(params).then((success) => {
-        success = success.body;
-        if (success.ok) {
-          list.splice(0, 1);
-          if (list.length) {
-            sendRec(list, onend);
-          }
-        } else {
-          alert(this.local.alert_something_wrong);
-          onend(success);
-        }
-
-        if (!list.length) {
-          onend(success);
-        }
-      });
-    }
-
-    sendRec(listToSend, (success) => {
-      data.onEnd.apply(null);
-      let post = data.post;
-      post.date = success.result.date;
-      post.edit_date = success.result.edit_date || null;
-      post.text = data.text || null;
-      post.parse_mode = params.parse_mode;
-      post.disableNotification = params.disable_notification;
-      post.disablePreview = params.disable_web_page_preview;
-      let posts = this.state.posts;
-      posts[this.getPostIndexByUid(data.post.uid)] = post;
-      localStorage.setItem('posts', JSON.stringify(posts));
-      this.setState({
-        posts: posts,
-      });
-    });
-
-    analytics.event('Main events', 'Used parser: ' + data.parser).send();
-  }
-
-  getPostIndexByUid(uid) {
-    for (let i = 0; i < this.state.posts.length; ++i) {
-      if (this.state.posts[i].uid === uid) return i;
-    }
-  }
-
-  getDraftIndexByUid(uid) {
-    for (let i = 0; i < this.state.drafts.length; ++i) {
-      if (this.state.drafts[i].uid === uid) return i;
-    }
-  }
-
-  newChannel(data) {
-    if (!data.name || data.name.length === 0)
-      return alert(this.local.alert_channel_name_short);
-
-    if (!data.username || !usernameRegex.test(data.username))
-      return alert(this.local.alert_invalid_channel_name);
-
-    let channels = this.state.channels;
-    channels.push({
-      name: data.name,
-      username: data.username,
-      uid: Utils.uid(),
-    });
-
+  closeSnackbar() {
     this.setState({
-      channels: channels,
-    }, () => localStorage.setItem('channels', JSON.stringify(channels)));
-  }
-
-  removeChannel(id, onRemove) {
-    let channels = this.state.channels;
-
-    for (let i = 0; i < channels.length; ++i) {
-      if (channels[i].uid === id) {
-        channels.splice(i, 1);
-        return this.setState({
-          channels: channels,
-        }, () => {
-          localStorage.setItem('channels', JSON.stringify(channels));
-          onRemove();
-        });
-      }
-    }
-  }
-
-  setAdminBot(token, onPass) {
-    if (!token || token.length < 30)
-      return alert(this.loca.alert_invalid_token);
-
-    if (!tokenRegex.test(token))
-      return alert(this.local.alert_invalid_token_format);
-
-    if (onPass) onPass();
-
-    this.API = new TGAPI(token);
-    let API = this.API;
-
-    let signal = this.signal;
-    signal.call('SetLoadState', [true]);
-
-    API.getMe().then((getMe) => {
-      getMe = getMe.body;
-      if (getMe.ok) {
-        getMe.result.token = token;
-
-        localStorage.setItem('bot', JSON.stringify(getMe.result));
-        this.token = token;
-        this.setState({
-          bot: getMe.result,
-        }, () => signal.call('SetLoadState', [false]));
-        this.updateBotProfilePhoto(getMe.result.id);
-      }
-    }, (err) => {
-      signal.call('SetLoadState', [false]);
-      alert(this.local.alert_cant_find_bot);
+      snackbar: {
+        open: false,
+        text: '',
+      },
     });
+  }
+
+  makeSnackbar(text) {
+    this.setState({
+      snackbar: {
+        open: true,
+        text,
+      },
+    });
+  }
+
+  postWriteDefaultsChange(value) {
+    const state = this.state;
+    state.settings.postWriteDefaults = value;
+    localStorage.setItem('settings', JSON.stringify(state.settings));
+    this.setState(state);
+  }
+
+  darkThemeChange(value) {
+    const state = this.state;
+    state.settings.darkTheme = value;
+    localStorage.setItem('settings', JSON.stringify(state.settings));
+    this.setState(state, this.props.deepForceUpdate);
+  }
+
+  languageChange(value) {
+    const state = this.state;
+    state.settings.lang = value;
+    localStorage.setItem('settings', JSON.stringify(state.settings));
+    this.local = langs[value];
+    this.setState(state);
+    ipcRenderer.send('build-menu', this.local);
   }
 
   updateBotProfilePhoto(id) {
-    id = id !== undefined ? id : this.props.bot.id;
+    const userId = id !== undefined ? id : this.props.bot.id;
 
-    let API = this.API;
+    const API = this.API;
 
     this.signal.call('SetAvatarLoadState', [true]);
-    this.API.getBase64Avatar(id).then((base64) => {
-      base64 = base64 === null ? './assets/botavatar.png' : base64;
-      localStorage.setItem('botavatar', base64);
+    API.getBase64Avatar(userId).then(res => {
+      const botavatar = res === null ? './assets/botavatar.png' : res;
+      localStorage.setItem('botavatar', botavatar);
       this.setState({
-        botavatar: base64,
+        botavatar,
       }, () => this.signal.call('SetAvatarLoadState', [false]));
     });
   }
@@ -350,62 +368,69 @@ class Chad extends React.Component {
   }
 
   deletePost(uid, onDelete) {
-    let posts = this.state.posts;
+    const posts = this.state.posts;
     for (let i = 0; i < posts.length; ++i) {
       if (posts[i].uid === uid) {
         posts.splice(i, 1);
         return this.setState({
-          posts: posts,
+          posts,
         }, () => {
           localStorage.setItem('posts', JSON.stringify(posts));
           onDelete();
         });
       }
     }
+    return null;
   }
 
   deleteDraft(uid, onDelete) {
-    let drafts = this.state.drafts;
+    const drafts = this.state.drafts;
     for (let i = 0; i < drafts.length; ++i) {
       if (drafts[i].uid === uid) {
         drafts.splice(i, 1);
         return this.setState({
-          drafts: drafts,
+          drafts,
         }, () => {
           localStorage.setItem('drafts', JSON.stringify(drafts));
           onDelete();
         });
       }
     }
+    return null;
   }
 
   saveDraft(data) {
-    if (data.text.length < 1)
+    if (data.text.length < 1) {
       return alert(this.local.alert_post_text_empty);
+    }
 
-    data.uid = Utils.uid2();
-    let drafts = this.state.drafts;
-    drafts.unshift(data);
+    const draftData = data;
+    draftData.uid = Utils.uid2();
+    const drafts = this.state.drafts;
+    drafts.unshift(draftData);
     this.setState({
-      drafts: drafts,
+      drafts,
     }, () => this.makeSnackbar(this.local.snackbar_draft_saved));
     localStorage.setItem('drafts', JSON.stringify(drafts));
+    return null;
   }
 
   changeDraft(data) {
-    if (data.text.length < 1)
+    if (data.text.length < 1) {
       return alert(this.local.alert_post_text_empty);
+    }
 
-    let drafts = this.state.drafts;
+    const drafts = this.state.drafts;
     drafts[this.getDraftIndexByUid(data.uid)] = data;
     this.setState({
-      drafts: drafts,
+      drafts,
     });
     localStorage.setItem('drafts', JSON.stringify(drafts));
+    return null;
   }
 
   render() {
-    let SideBarData = {
+    const SideBarData = {
       lang: this.state.settings.lang,
       darkTheme: this.state.settings.darkTheme,
       postWriteDefaults: this.state.settings.postWriteDefaults,
@@ -413,12 +438,12 @@ class Chad extends React.Component {
       bot: {
         avatar: this.state.botavatar,
         name: this.state.bot.first_name,
-        username: '@' + this.state.bot.username,
+        username: `@${this.state.bot.username}`,
         token: this.token,
       },
     };
 
-    let ContentBarData = {
+    const ContentBarData = {
       posts: this.state.posts,
       drafts: this.state.drafts,
       postWriteDefaults: this.state.settings.postWriteDefaults,
@@ -428,7 +453,7 @@ class Chad extends React.Component {
 
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
-        <Layout type='column' style={{ backgroundColor: theme.palette.canvasColor }}>
+        <Layout type="column" style={{ backgroundColor: theme.palette.canvasColor }}>
           <Flex>
             <Layout type="row">
               <Fixed>
@@ -436,14 +461,16 @@ class Chad extends React.Component {
                   <SideBar
                     signal={this.signal}
                     data={SideBarData}
-                    local={this.local}/>
+                    local={this.local}
+                  />
                 </Paper>
               </Fixed>
               <Flex style={tags.contentStyle}>
                 <ContentBar
                   data={ContentBarData}
                   local={this.local}
-                  signal={this.signal}/>
+                  signal={this.signal}
+                />
               </Flex>
             </Layout>
           </Flex>
@@ -451,11 +478,16 @@ class Chad extends React.Component {
             open={this.state.snackbar.open}
             message={this.state.snackbar.text}
             autoHideDuration={4000}
-            onRequestClose={this.closeSnackbar}/>
+            onRequestClose={this.closeSnackbar}
+          />
         </Layout>
       </MuiThemeProvider>
     );
   }
 }
+Chad.propTypes = {
+  deepForceUpdate: React.PropTypes.func,
+  bot: React.PropTypes.object,
+};
 
 module.exports = Chad;
--- src/content/main.jsx
+++ src/content/main.jsx
@@ -2,12 +2,14 @@ const overlay = document.getElementById('overlay');
 let instance = null;
 
 setTimeout(() => {
-  window.React = require('react');
+  /* eslint-disable global-require */
+  const React = require('react');
   const { render } = require('react-dom');
   const injectTapEventPlugin = require('react-tap-event-plugin');
   const deepForceUpdate = require('react-deep-force-update');
-  const Velocity = require('velocity-animate');
+  const velocity = require('velocity-animate');
   require('./js/DataRepair')();
+  /* eslint-enable global-require */
 
   injectTapEventPlugin();
 
@@ -15,12 +17,14 @@ setTimeout(() => {
     deepForceUpdate(instance);
   }
 
+  /* eslint-disable global-require */
   const Chad = require('./Chad');
+  /* eslint-enable global-require */
 
   instance = render(
-    <Chad deepForceUpdate={deepUpdate}/>,
+    <Chad deepForceUpdate={deepUpdate} />,
     document.getElementById('container')
   );
 
-  Velocity(overlay, 'fadeOut', { duration: 500 });
+  velocity(overlay, 'fadeOut', { duration: 500 });
 }, 10);
